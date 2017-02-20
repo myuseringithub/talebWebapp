445bb1610e5645e9a643f3934c0a7e911b386982
MyUserInGithub <sfmissive@gmail.com>
2017-02-20 21:27:46 +0200

    Upload examples.

--- setup/run.sh
+++ setup/run.sh
@@ -2,6 +2,25 @@
 
 production() {
 
+    # 1.
+    docker-machine ssh $VM-1
+    VolumeBasePath=/mnt/datadisk-1/rethinkdb
+    sudo mkdir -p $VolumeBasePath/rethinkdbData
+
+    # 2.
+    docker stack deploy -c ./setup/container/production.dockerStack.yml mobta3athWebapp
+}
+
+development() {
+
+    # DOESN'T WORK
+    # fix issue caused by virtualbox & Windows apparently that don't support sync directory - https://forums.docker.com/t/issues-with-rethinkdb-example-using-volumes/13720/4
+    # Download exe and run exe - https://www.rethinkdb.com/docs/install/windows/
+    # grab the files created in new directory to the volume that will be mounted to the container.
+
+    # . 
+    export DEPLOYMENT=development
+    docker-compose -f ./setup/container/development.dockerCompose.yml up -d 
 }
 
 # Important: call arguments verbatim. i.e. allows first argument to call functions inside file. So that it could be called as "./setup/run.sh <functionName>".
