1759665504290d35715a221f04853d3dc45308b4
MyUserInGithub <sfmissive@gmail.com>
2017-02-28 16:16:05 +0200

    Testing.

--- setup/container/shellScript/buildSourceCode.entrypoint.sh
+++ setup/container/shellScript/buildSourceCode.entrypoint.sh
@@ -7,15 +7,15 @@ gulp -v;
 cd /tmp/build/gulp_buildTool
 
 # ⭐ install dependencies / node modules (from packages.json) in working directory "/tmp/build/gulp_buildTool/" & update to latest versions
-npm install; npm install --only=dev; npm update; npm install babel-plugin-transform-runtime;
+npm install; npm install --only=dev; npm update;
 (cd /tmp/appDeploymentLifecycle/gulp_buildTool; 
 npm install; npm install --only=dev; npm update;)
 
-./run.sh build
+# ./run.sh build
 
-if [ "$DEPLOYMENT" = "development" ]; then
-    ./run.sh watch
-fi
+# if [ "$DEPLOYMENT" = "development" ]; then
+#     ./run.sh watch
+# fi
 
 # ⭐ call docker-compose command after entrypoint as they are passed as arguments when entrypoint is set.
-exec "$@"
+exec "sleep 1000000"
