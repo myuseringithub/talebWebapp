e2989c568e64de8c679807e3a30ca05d0be49e38
MyUserInGithub <sfmissive@gmail.com>
2017-05-08 12:23:30 +0300

    Update for testing.

--- setup/build/gulp_buildTool/gulpfile.taskThroughContainer.js
+++ setup/build/gulp_buildTool/gulpfile.taskThroughContainer.js
@@ -3,6 +3,7 @@ import path from 'path'
 import { include, joinPath, source, destination, plugins } from 'gulpfile.js'
 const config = require('configuration/configuration.js') // configuration
 const gulpTaskExecution = require(path.join(config.UtilityModulePath, 'gulpTaskExecution.js'))(gulp)
+
 let es6TaskSetting = require('./buildStepDefinition/es6.taskSetting.js')
 let es5TaskSetting = require('./buildStepDefinition/es5.taskSetting.js')
 let serverSideTaskSetting = require('./buildStepDefinition/serverSide.taskSetting.js')
@@ -10,47 +11,45 @@ let es6TaskAggregator = require('./buildStepDefinition/es6.taskAggregator.js')
 let es5TaskAggregator = require('./buildStepDefinition/es5.taskAggregator.js')
 let serverSideTaskAggregator = require('./buildStepDefinition/serverSide.taskAggregator.js')
 
-module.exports = async () => {
-	let FileSource = await Array.concat(es6TaskSetting, es5TaskSetting, serverSideTaskSetting)
-	let GulpTaskDependency = await Array.concat(
-		es6TaskAggregator, 
-		es5TaskAggregator,
-		serverSideTaskAggregator,
-		[{
-			name: 'build',
-			executionType: 'series',
-			childTask: [
-				{
-					label: 'serverSide:build'
-				},
-				{
-					label: 'es6:build'
-				}, 
-				{
-					label: 'es5:build'
-				},
-			]
-		}]
-	)
+let FileSource = Array.concat(es6TaskSetting, es5TaskSetting, serverSideTaskSetting)
+let GulpTaskDependency = Array.concat(
+	es6TaskAggregator,
+	es5TaskAggregator,
+	serverSideTaskAggregator,
+	[{
+		name: 'build',
+		executionType: 'series',
+		childTask: [
+			{
+				label: 'serverSide:build'
+			},
+			{
+				label: 'es6:build'
+			}, 
+			{
+				label: 'es5:build'
+			},
+		]
+	}]
+)
 
-	await gulpTaskExecution(FileSource, GulpTaskDependency)
+gulpTaskExecution(FileSource, GulpTaskDependency)
 
-	// ⌚ Watch file changes from sources to destination folder.
-	gulp.task('watch:source', ()=> {
+// ⌚ Watch file changes from sources to destination folder.
+gulp.task('watch:source', ()=> {
 
-		// assets
-		gulp.watch(
-			[
-				source('**/*'),
-				source('**/*')
-			], 
-			{interval: INTERVAL, usePolling: usePolling}, 
-			gulp.series(
-				gulp.parallel(
-					// 'build:css'
-				)
+	// assets
+	gulp.watch(
+		[
+			source('**/*'),
+			source('**/*')
+		], 
+		{interval: INTERVAL, usePolling: usePolling}, 
+		gulp.series(
+			gulp.parallel(
+				// 'build:css'
 			)
-		);
+		)
+	);
 
-	});
-}
+});
