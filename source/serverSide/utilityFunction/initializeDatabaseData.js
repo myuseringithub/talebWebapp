aaf263444264a6993e01f9a2dd52e456f98f27ad
Dr. Safi <sfmissive@gmail.com>
2017-12-13 11:36:25 +0200

    Update moved database models into common utility folder.

--- source/serverSide/utilityFunction/initializeDatabaseData.js
+++ source/serverSide/utilityFunction/initializeDatabaseData.js
@@ -1,41 +1,16 @@
-import rethinkDB from 'rethinkdb' 
 import { default as Application } from 'appscript'
 import {default as getTableDocumentDefault} from "appscript/utilityFunction/database/query/getTableDocument.query.js";
+import { createDatabase, createTableAndInsertData } from "appscript/utilityFunction/database/initializeDatabase.query.js";
 import databaseData from 'databaseDefaultData/databaseData.js'
 
 function initializeDatabaseData() {
     return () => {
         const connection = Application.rethinkdbConnection
-        async function createDatabase(databaseName) {
-            let databaseExists = await rethinkDB.dbList().contains(databaseName).run(connection);
-            if(!databaseExists) {
-                let dbCreationResponse = await rethinkDB.dbCreate(databaseName).run(connection)
-                if(dbCreationResponse.dbs_created > 0)  console.log(`📢 ${databaseName} database created !`)
-            } else {
-                console.log(`📢📁 ${databaseName} database already exists !`)            
-            }
-        }
-
-        async function createTableAndInsertData(databaseName, databaseData) {
-            for (let tableData of databaseData) {
-                await rethinkDB.db(databaseName).tableCreate(tableData.databaseTableName).run(connection)
-                    .then(async tableCreationResponse => {
-                        if(tableCreationResponse.tables_created > 0) console.log(`📢 ${tableData.databaseTableName} table created.`)
-                        await rethinkDB.db(databaseName).table(tableData.databaseTableName).insert(tableData.data).run(connection)
-                            .then(response => {
-                                console.log(`📢📥 ${response.inserted} documents inserted to ${tableData.databaseTableName}.`)
-                            })
-                            .catch(error => console.log(error))
-                    })
-                    .catch(error => console.log(`📢 ${tableData.databaseTableName} table already exists.`))
-            }
-        }
-
         
-        createDatabase('webappSetting')
+        createDatabase('webappSetting', connection)
             .then(async () => {
                 try {
-                    await createTableAndInsertData('webappSetting', databaseData.webappSetting)
+                    await createTableAndInsertData('webappSetting', databaseData.webappSetting, connection)
                 } catch (error) {
                     console.log('webappSetting - cannot create table / insert data for webappSetting')
                     console.log(error)
@@ -62,23 +37,16 @@ function initializeDatabaseData() {
                 }    
             })
 
-        createDatabase('webappContent')
+        createDatabase('webappContent', connection)
             .then(() => {
                 try {
-                    createTableAndInsertData('webappContent', databaseData.webappContent)            
+                    createTableAndInsertData('webappContent', databaseData.webappContent, connection)            
                 } catch (error) {
                     console.log('webappContent - cannot create table / insert data for webappContent')
                     console.log(error)
                 }
             })
 
-        // .do(function(databaseExists) {
-        //   return rethinkDB.branch(
-        //     databaseExists,
-        //     { dbs_created: 0 },
-        //     rethinkDB.dbCreate('webapp')
-        //   );
-        // })
     }
 }
 
