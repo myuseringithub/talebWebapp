bb4f0f3284776bceeeea76273cc5bfe2f87c2e3f
Safi <nassarsafi@gmail.com>
2019-10-20 20:21:47 +0300

    ⚗ Small configuration updates.

--- source/serverSide/entrypoint.js
+++ source/serverSide/entrypoint.js
@@ -1,6 +1,6 @@
 const moduleSystem = require('module')
 const path = require('path')
-const configuration = require('../../setup/configuration/configuration.js')
+const configuration = require('../../configuration')
 
 const appRootPath = path.normalize(__dirname)
 // add root path (app base path) to the resolved module paths.
